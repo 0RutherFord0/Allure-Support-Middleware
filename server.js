e1e04b0a9deb1a4b37963784e4d04469d9699441
Aditya Tawade <adityatwd0@gmail.com>
2024-04-10 16:17:29 +0530

    Order Change

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = 3000; // Port number for the server
 // Define a route for '/order'
 app.get("/order", (req, res) => {
   // Return JSON response with the number 1
-  res.json({ order: 2 });
+  res.json({ order: 3 });
 });
 
 // Start the server
