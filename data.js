6eeeea84bf48196eee7a0cf44a5d787ca6d9ee5b
Aditya Tawade <adityatwd0@gmail.com>
2024-04-11 14:02:14 +0530

    Change Data

--- data.js
+++ data.js
@@ -12,15 +12,17 @@ const orders = [
   {
     orderId: 73,
     emailAddress: "kishore@iconnectsolutions.com",
-    imageURL: "https://example.com/image73.jpg",
-    title: "Product B",
+    imageURL:
+      "https://i.pinimg.com/736x/b0/8b/75/b08b75c684baf6773907ef120038c8c1.jpg",
+    title: "Men's Tuxedo Trousers",
     deliveryDate: "2024-04-18",
   },
   {
     orderId: 75,
     emailAddress: "kishore@iconnectsolutions.com",
-    imageURL: "https://example.com/image75.jpg",
-    title: "Product C",
+    imageURL:
+      "https://www.mohanlalsons.com/cdn/shop/files/SWAT-INJ2-MLS-9162-WINE_2.jpg?v=1711725321",
+    title: "MLS Indo Western 2pcs",
     deliveryDate: "2024-04-20",
   },
 ];
