aaaaf5fae7aba51e3e703c6959feb042156cd9c8
Aditya Tawade <adityatwd0@gmail.com>
2024-04-11 14:05:05 +0530

    Added 2 Image

--- data.js
+++ data.js
@@ -2,15 +2,7 @@
 
 const orders = [
   {
-    orderId: 67,
-    emailAddress: "kishore@iconnectsolutions.com",
-    imageURL:
-      "https://media.istockphoto.com/id/507842306/photo/man-in-tuxedo-and-bow-tie.jpg?s=612x612&w=0&k=20&c=V_RmlyO1tAshHrEzbgi5hHaiBdYnN5Rby9OioSQXD-o=",
-    title: "Exclusive Fit 3pc Dark Blue Suit",
-    deliveryDate: "2024-04-15",
-  },
-  {
-    orderId: 73,
+    orderId: 7332,
     emailAddress: "kishore@iconnectsolutions.com",
     imageURL:
       "https://i.pinimg.com/736x/b0/8b/75/b08b75c684baf6773907ef120038c8c1.jpg",
@@ -18,7 +10,15 @@ const orders = [
     deliveryDate: "2024-04-18",
   },
   {
-    orderId: 75,
+    orderId: 6786,
+    emailAddress: "kishore@iconnectsolutions.com",
+    imageURL:
+      "https://media.istockphoto.com/id/507842306/photo/man-in-tuxedo-and-bow-tie.jpg?s=612x612&w=0&k=20&c=V_RmlyO1tAshHrEzbgi5hHaiBdYnN5Rby9OioSQXD-o=",
+    title: "Exclusive Fit 3pc Dark Blue Suit",
+    deliveryDate: "2024-04-15",
+  },
+  {
+    orderId: 7325,
     emailAddress: "kishore@iconnectsolutions.com",
     imageURL:
       "https://www.mohanlalsons.com/cdn/shop/files/SWAT-INJ2-MLS-9162-WINE_2.jpg?v=1711725321",
